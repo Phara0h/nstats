45feb2ce5c9105ab4988fc503f8483a05cc54aa7
jwhissel <jt.whissel@riptidesoftware.com>
2017-01-19 15:08:09 -0500

    Converted nstats to be an ES6 class while making it avoid root hoisting

--- nstats.js
+++ nstats.js
@@ -1,141 +1,166 @@
-//clients are the websocket clients
-var _ = require('underscore')._;
+class NStats
+{
 
-function Nstats(clients) {
+  constructor(clients)
+  {
+    this.clients = clients;
+    this.lastCalc = 0;
 
-  var stats = this;
-  var lastCalc = 0;
+    this.interval = 1000;
+    this.data = {
+      uptime: 0,
+      totalMemory: 0,
+      totalOnlineUsers: 0,
+      avgWriteKBs: 0,
+      avgReadKBs: 0,
+      avgPacketsSecond: 0,
+      bytesWritten: 0,
+      totalBytesWritten: 0,
+      totalMBytesWritten: 0,
+      bytesRead: 0,
+      totalBytesRead: 0,
+      totalMBytesRead: 0,
+      writeKBS: 0,
+      readKBS: 0,
+      totalPackets: 0
+    };
+    this.calc();
+  }
 
-  stats.interval = 1000;
-  stats.data = {
-    uptime: 0,
-    totalMemory: 0,
-    totalOnlineUsers: 0,
-    avgWriteKBs: 0,
-    avgReadKBs: 0,
-    avgPacketsSecond: 0,
-    bytesWritten: 0,
-    totalBytesWritten: 0,
-    totalMBytesWritten: 0,
-    bytesRead: 0,
-    totalBytesRead: 0,
-    totalMBytesRead: 0,
-    writeKBS: 0,
-    readKBS: 0,
-    totalPackets: 0
+
+
+  express(req, res, next)
+  {
+    return (req, res, next) => {
+
+       if (req.session) {
+         if (req.session.stats) {
+           this.data.bytesRead += (req.socket.bytesRead - req.session.this.bytesRead) > 0 ? req.socket.bytesRead - req.session.this.bytesRead : 0;
+           this.data.bytesWritten += (req.socket.bytesWritten - req.session.this.bytesWritten) > 0 ? req.socket.bytesWritten - req.session.this.bytesWritten : 0;
+         }
+
+         req.session.stats = {
+           bytesRead: req.socket.bytesRead,
+           bytesWritten: req.socket.bytesWritten
+         };
+
+         this.data.totalPackets++;
+       } else {
+         this.addWeb(req);
+       }
+       next();
+   };
+  }
+
+  addWeb(req)
+  {
+    this.data.bytesRead += req.socket.bytesRead;
+    this.data.bytesWritten += req.socket.bytesWritten;
+    this.data.totalPackets++;
   };
 
-
-  stats.express = function(req, res, next) {
-
-    if (req.session) {
-      if (req.session.stats) {
-        stats.data.bytesRead += (req.socket.bytesRead - req.session.stats.bytesRead) > 0 ? req.socket.bytesRead - req.session.stats.bytesRead : 0;
-        stats.data.bytesWritten += (req.socket.bytesWritten - req.session.stats.bytesWritten) > 0 ? req.socket.bytesWritten - req.session.stats.bytesWritten : 0;
-      }
-
-      req.session.stats = {
-        bytesRead: req.socket.bytesRead,
-        bytesWritten: req.socket.bytesWritten
-      };
-
-      stats.data.totalPackets++;
-    } else {
-      stats.addWeb(req);
-    }
-    next();
+  toJson()
+  {
+    return JSON.stringify(this.data);
   };
 
-  stats.addWeb = function(req) {
-    stats.data.bytesRead += req.socket.bytesRead;
-    stats.data.bytesWritten += req.socket.bytesWritten;
-    stats.data.totalPackets++;
-  };
-
-  stats.toJson = function() {
-    return JSON.stringify(stats.data);
-  };
-
-  function ConvertTime(timeInSeconds) {
+  ConvertTime(timeInSeconds)
+  {
     var sec_num = parseInt(timeInSeconds, 10);
     var hours = Math.floor(sec_num / 3600);
 
     var minutes = Math.floor((sec_num - (hours * 3600)) / 60);
     var seconds = sec_num - (hours * 3600) - (minutes * 60);
 
-    if (hours < 10) {
+    if (hours < 10)
+    {
       hours = "0" + hours;
     }
-    if (minutes < 10) {
+    if (minutes < 10)
+    {
       minutes = "0" + minutes;
     }
-    if (seconds < 10) {
+    if (seconds < 10)
+    {
       seconds = "0" + seconds;
     }
     var time = hours + ':' + minutes + ':' + seconds;
     return time;
   }
 
-  stats.calc = function(cb){
-      process.nextTick(function() {
-        var w = 0;
-        var r = 0;
+  calc(cb)
+  {
+    process.nextTick(() =>
+    {
+      var w = 0;
+      var r = 0;
 
-        for (var i in clients) {
-          if (clients[i] !== null) {
-            try {
-              if (clients[i]._socket !== null) {
-                var tW = Math.abs(Number(clients[i]._socket.bytesWritten));
-                if (_.isNumber(tW))
-                  w += tW;
-              }
+      for (var i in this.clients)
+      {
+        if (this.clients[i] !== null)
+        {
+          try
+          {
+            if (this.clients[i]._socket !== null)
+            {
+              var tW = Math.abs(Number(this.clients[i]._socket.bytesWritten));
+              if (_.isNumber(tW))
+                w += tW;
+            }
 
-              if (clients[i]._socket !== null) {
-                var tR = Math.abs(Number(clients[i]._socket.bytesRead));
+            if (this.clients[i]._socket !== null)
+            {
+              var tR = Math.abs(Number(this.clients[i]._socket.bytesRead));
 
-                if (_.isNumber(tR))
-                  r += tR;
-              }
-            } catch (e) {
-              console.log(e);
+              if (_.isNumber(tR))
+                r += tR;
             }
           }
+          catch (e)
+          {
+            console.log(e);
+          }
         }
+      }
 
-        if(lastCalc > 0)
-          lastCalc = Number(((_.now() - lastCalc)) / 1000);
+      if (this.lastCalc > 0)
+        this.lastCalc = Number(((Date.now() - this.lastCalc)) / 1000);
 
-        stats.data.writeKBS = Number(Math.abs(((w - stats.data.bytesWritten) / 1024)/lastCalc)).toFixed(2);
-        stats.data.readKBS = Number(Math.abs(((r - stats.data.bytesRead) / 1024)/lastCalc)).toFixed(2);
+      this.data.writeKBS = Number(Math.abs(((w - this.data.bytesWritten) / 1024) / this.lastCalc)).toFixed(2);
+      this.data.readKBS = Number(Math.abs(((r - this.data.bytesRead) / 1024) / this.lastCalc)).toFixed(2);
 
-        stats.data.totalBytesRead += Math.abs(r - stats.data.bytesRead);
-        stats.data.totalBytesWritten += Math.abs(w - stats.data.bytesWritten);
-        stats.data.totalMBytesRead = Number((stats.data.totalBytesRead / 1048576).toFixed(2));
-        stats.data.totalMBytesWritten = Number((stats.data.totalBytesWritten / 1048576).toFixed(2));
-        stats.data.bytesWritten = w;
-        stats.data.bytesRead = r;
-        stats.data.avgWriteKBs = Number((stats.data.totalBytesWritten / 1024) / (process.uptime())).toFixed(2);
-        stats.data.avgReadKBs = Number((stats.data.totalBytesRead / 1024) / (process.uptime())).toFixed(2);
+      this.data.totalBytesRead += Math.abs(r - this.data.bytesRead);
+      this.data.totalBytesWritten += Math.abs(w - this.data.bytesWritten);
+      this.data.totalMBytesRead = Number((this.data.totalBytesRead / 1048576).toFixed(2));
+      this.data.totalMBytesWritten = Number((this.data.totalBytesWritten / 1048576).toFixed(2));
+      this.data.bytesWritten = w;
+      this.data.bytesRead = r;
+      this.data.avgWriteKBs = Number((this.data.totalBytesWritten / 1024) / (process.uptime())).toFixed(2);
+      this.data.avgReadKBs = Number((this.data.totalBytesRead / 1024) / (process.uptime())).toFixed(2);
 
 
-        stats.data.uptime = ConvertTime(process.uptime());
-        stats.data.totalMemory = Number(process.memoryUsage().rss / 1048576).toFixed(2);
-        stats.data.totalOnlineUsers = (clients != null ? clients.length : 0);
-        stats.data.avgPacketsSecond = (stats.data.totalPackets / (process.uptime())).toFixed(2);
+      this.data.uptime = this.ConvertTime(process.uptime());
+      this.data.totalMemory = Number(process.memoryUsage().rss / 1048576).toFixed(2);
+      this.data.totalOnlineUsers = (this.clients != null ? this.clients.length : 0);
+      this.data.avgPacketsSecond = (this.data.totalPackets / (process.uptime())).toFixed(2);
 
-        if(cb)
-          cb();
+      if(cb)
+      {
+        cb();
+      }
 
-        if(stats.interval > 0)
-          setTimeout(stats.calc, stats.interval);
+      if (this.interval > 0)
+      {
+        setTimeout(this.calc, this.interval);
+      }
 
-        lastCalc = _.now();
+      this.lastCalc = Date.now();
     });
   };
-  stats.calc();
 
-
-  return stats;
 }
 
-module.exports = Nstats;
+module.exports = function(clients)
+{
+  return new NStats(clients || null)
+};
